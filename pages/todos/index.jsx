7ff7decb269235fa5fe207c0ff2fd95cfe30e2a3
ArthurRodrigomarques <ArthurRodrigomarques@users.noreply.github.com>
2023-04-01 18:48:17 -0300

    att

--- pages/todos/index.jsx
+++ pages/todos/index.jsx
@@ -1,4 +1,5 @@
-import styles from '../styles/Todo.module.css'
+import Link from 'next/link'
+import styles from '../../styles/Todo.module.css'
 
 export async function getStaticProps() {
     const data = await fetch('https://jsonplaceholder.typicode.com/todos')
@@ -16,8 +17,11 @@ export default function Todos({ todos }) {
            <h1 className={styles.h1}>Tarefas para fazer</h1> 
            <ul className={styles.todolist}>
                 {todos.map((todo) => (
-                    <li key={todo.id}>{todo.title}</li>
+                    <div>
+                    <li key={todo.id}>{todo.title} - <Link href={`/todos/${todo.id}`}>Ver mais</Link></li>
+                    </div>
                 ))}
+                
            </ul>
         </>
     )
